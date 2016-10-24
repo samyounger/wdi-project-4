07767ebfcd1d5468d1c5c9966e366df94716d9d2
samyounger <sam_younger@hotmail.com>
2016-10-24 09:59:49 +0100

    restructuring files splitting out API & front-end to different git repos

--- back-end/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  scope :api do
-    resources :users
-    resources :companies
-    post 'register', to: 'authentications#register'
-    post 'login', to: 'authentications#login'
-  end
-end
